9443463f0a8491433362cba20ab4a6634e6e4ea2
unknown <jacob.e.england@gmail.com>
2020-04-10 11:26:10 -0500

    Added function to get all actions

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -8,6 +8,18 @@ const router = express.Router();
 
 // endpoints
 // ----- BASE URL: /api/actions -----
+router.get('/', (req, res) => {
+    db.get()
+        .then((actions) => {
+            res.status(200).json({ actions })
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({
+                message: 'Could not retrieve actions.'
+            })
+        })
+})
 
 // custom middleware
 function validateAction(req, res, next) {
@@ -26,6 +38,26 @@ function validateAction(req, res, next) {
             message: 'Action is missing description and/or notes.'
         })
     }
+}
+
+function validateActionId(req, res, next) {
+    id = req.params.id;
+    db.get(id)
+        .then((valid) => {
+            if (valid) {
+                next();
+            } else (
+                res.status(400).json({
+                    message: 'Action ID invalid.'
+                })
+            )
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({
+                message: 'Could not validate action ID.'
+            })
+        })
 }
 
 module.exports = router;
